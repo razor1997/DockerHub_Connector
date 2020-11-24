b41cf961af625661e6460132ed81af161eac008a
razor1997 <webmastering1997@gmail.com>
2020-11-24 21:41:50 +0100

    Add files via upload

--- src/main/java/com/mycompany/pwcho/App.java
+++ src/main/java/com/mycompany/pwcho/App.java
@@ -55,38 +55,38 @@ public class App {
         String query = "SELECT * FROM human";
         ResultSet rs = statement.executeQuery(query);
         while (rs.next()){
-            System.out.println("|"+rs.getString("human_id")+"|=  "+rs.getString("Imie") + " | "  + rs.getString("Nazwisko")+ " | "  +rs.getString("Wiek") );
+            System.out.println("|"+rs.getString("human_id")+"|=  "+rs.getString("Name") + " | "  + rs.getString("Subname")+ " | "  +rs.getString("Age") );
         }
         DisplayMenu();
     }
     public static void saveHuman() throws SQLException {
         Scanner in = new Scanner(System.in);
         System.out.println("Name : ");
-        String imie = in.nextLine();
+        String name = in.nextLine();
         System.out.println("SubName: ");
-        String nazwisko = in.nextLine();
+        String subname = in.nextLine();
         System.out.println("Age ");
-        int wiek = in.nextInt();
+        int age = in.nextInt();
 
         DBHelper dbHelper = new DBHelper();
 
-        String sql_stmt = "INSERT INTO human (imie, nazwisko, wiek) VALUES ('"+ imie+"','" +nazwisko +"',"+ wiek + ")";
+        String sql_stmt = "INSERT INTO human (name, subname, age) VALUES ('"+ name+"','" +subname +"',"+ age + ")";
 
         dbHelper.ExecuteSQLStatement(sql_stmt);
-        System.out.println("Dodano do bazy cz?owieka o danych takich jak : "+imie + " " + nazwisko + " "+ wiek);
+        System.out.println("Added users: "+name + " " + subname + " "+ age);
         DisplayMenu();
     }
 
     public static void deleteHuman() throws SQLException {
         Scanner in = new Scanner(System.in);
-        System.out.println("Wpisz ID do usuniecia: ");
+        System.out.println("Get ID to delete: ");
         int id = in.nextInt();
         DBHelper dbHelper = new DBHelper();
 
         String sql_stmt = "DELETE FROM human WHERE human_id = "+id+"";
 
         dbHelper.ExecuteSQLStatement(sql_stmt);
-        System.out.println("Cz?owiek o ID : "+id+ " zosta? usuniety");
+        System.out.println("User ID : "+id+ " deleted");
         DisplayMenu();
     }
     public static void updateHuman() throws SQLException {
@@ -95,15 +95,15 @@ public class App {
         System.out.println("Give ID : ");
         int id = Integer.parseInt(updateScanner.nextLine());
         System.out.println("Name: ");
-        String imie = updateScanner.nextLine();
+        String name = updateScanner.nextLine();
         System.out.println("SubName: ");
-        String nazwisko = updateScanner.nextLine();
+        String subname = updateScanner.nextLine();
         System.out.println("Age ");
-        int wiek = updateScanner.nextInt();
+        int age = updateScanner.nextInt();
 
 
         DBHelper dbHelper = new DBHelper();
-        String sql_stmt = "UPDATE human SET imie = '"+imie+"', nazwisko = '"+nazwisko+"', wiek ="+ wiek +" WHERE human_id = " + id;
+        String sql_stmt = "UPDATE human SET imie = '"+name+"', nazwisko = '"+subname+"', wiek ="+ age +" WHERE human_id = " + id;
         dbHelper.ExecuteSQLStatement(sql_stmt);
         System.out.println("Id User : "+id+ " is update");
         DisplayMenu();
